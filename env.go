b763404df033b7e37b14b8e270e1478b38c9de05
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-08-30 21:03:34 +0800

    update env
    add once func

--- env.go
+++ env.go
@@ -3,41 +3,47 @@ package errors
 import (
 	"os"
 	"strings"
+	"sync"
 )
 
+var gopaths = make([]string, 0, 2)
+
+var _gopathOnce = sync.Once{}
+
 func goEnv() []string {
-	env := make([]string, 0, 2)
-	// goroot
-	goroot := os.Getenv("GOROOT")
-	if len(goroot) > 0 {
-		if strings.Contains(goroot, `\`) && strings.Contains(goroot, ":") { // win
-			goroot = strings.Replace(goroot, `\`, "/", -1)
-		}
-		env = append(env, goroot)
-	}
-	gopath := os.Getenv("GOPATH")
-	if len(gopath) == 0 {
-		return env
-	}
-	if strings.Contains(gopath, `\`) && strings.Contains(gopath, ":") { // win
-		gopath = strings.Replace(gopath, `\`, "/", -1)
-		if strings.Contains(gopath, ";") {
-			gopaths := strings.Split(gopath, ";")
-			for _, item := range gopaths {
-				env = append(env, strings.TrimSpace(item))
+	_gopathOnce.Do(func() {
+		// goroot
+		goroot := os.Getenv("GOROOT")
+		if len(goroot) > 0 {
+			if strings.Contains(goroot, `\`) && strings.Contains(goroot, ":") { // win
+				goroot = strings.Replace(goroot, `\`, "/", -1)
 			}
-		} else {
-			env = append(env, strings.TrimSpace(gopath))
+			gopaths = append(gopaths, goroot)
 		}
-	} else { // unix
-		if strings.Contains(gopath, ":") {
-			gopaths := strings.Split(gopath, ":")
-			for _, item := range gopaths {
-				env = append(env, strings.TrimSpace(item))
+		gopath := os.Getenv("GOPATH")
+		if len(gopath) == 0 {
+			return
+		}
+		if strings.Contains(gopath, `\`) && strings.Contains(gopath, ":") { // win
+			gopath = strings.Replace(gopath, `\`, "/", -1)
+			if strings.Contains(gopath, ";") {
+				gopaths := strings.Split(gopath, ";")
+				for _, item := range gopaths {
+					gopaths = append(gopaths, strings.TrimSpace(item))
+				}
+			} else {
+				gopaths = append(gopaths, strings.TrimSpace(gopath))
 			}
-		} else {
-			env = append(env, strings.TrimSpace(gopath))
+		} else { // unix
+			if strings.Contains(gopath, ":") {
+				gopaths := strings.Split(gopath, ":")
+				for _, item := range gopaths {
+					gopaths = append(gopaths, strings.TrimSpace(item))
+				}
+			} else {
+				gopaths = append(gopaths, strings.TrimSpace(gopath))
+			}
 		}
-	}
-	return env
+	})
+	return gopaths
 }
